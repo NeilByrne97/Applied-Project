b1fda5a86f88372e7d710ea97a4e1cfbbf2752c5
Neil Byrne <G00343624@gmit.ie>
2020-11-12 16:14:43 +0000

    Created add contact component

--- CovidTracker/contactlist/client/src/app/contacts/contacts.component.ts
+++ CovidTracker/contactlist/client/src/app/contacts/contacts.component.ts
@@ -18,6 +18,19 @@ export class ContactsComponent implements OnInit {
 
   constructor(private contactService: ContactService) { }
 
+  addContact(){
+    const newContact ={
+      first_name: this.first_name,
+      last_name: this.last_name,
+      phone: this.phone
+    }
+    this.contactService.addContact(newContact)
+    .subscribe(contact =>{
+      this.contacts.push(contact);
+    });
+  }
+
+
   deleteContact(id:any){
     var contacts = this.contacts;
     this.contactService.deleteContact(id)
@@ -29,7 +42,7 @@ export class ContactsComponent implements OnInit {
               }
             }
           }
-      })
+      });
   }
 
   ngOnInit(): void {
