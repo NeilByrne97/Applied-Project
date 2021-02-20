fc554febfab1d8398b34b5d95b624b737a2f0795
Neil Byrne <G00343624@gmit.ie>
2021-02-20 16:43:56 +0000

    Added contact forum to android app

--- android_bluetooth_serial_app/lib/main.dart
+++ android_bluetooth_serial_app/lib/main.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 
 import './MainPage.dart';
 
-void main() => runApp(new ExampleApplication());
+void main() => runApp(new Application(
 
-class ExampleApplication extends StatelessWidget {
+));
+
+class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.blue,
+        accentColor: Colors.cyan
+    );
     return MaterialApp(home: MainPage());
   }
 }
