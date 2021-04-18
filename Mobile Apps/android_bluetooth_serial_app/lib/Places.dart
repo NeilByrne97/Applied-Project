102debee5e64f3f938cfcf2ec46f1bedb15b2ede
Neil Byrne <G00343624@gmit.ie>
2021-04-18 19:11:12 +0100

    Google placesID working, implenting it with Firebase

--- Mobile Apps/android_bluetooth_serial_app/lib/Places.dart
+++ Mobile Apps/android_bluetooth_serial_app/lib/Places.dart
@@ -1,32 +1,106 @@
 import 'dart:io';
+import 'dart:typed_data';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_webservice/geocoding.dart';
 import 'package:google_maps_webservice/places.dart';
+import 'package:google_place/google_place.dart';
 
 class PlacesDetails extends StatefulWidget {
 
   final String apiKey = 'AIzaSyAz6TJpPOpuhahblOebTaiCmtXHcipwxjc';
   @override
   _PlacesDetailsState createState() => _PlacesDetailsState();
+
 }
 class _PlacesDetailsState extends State<PlacesDetails> {
+  String name, formattedAddress, formattedPhoneNumber, website, photos;
+
+  getName(name) {
+    this.name = name;
+  }
+
+  getFormattedAddress(formattedAddress) {
+    this.formattedAddress = formattedAddress;
+  }
+
+  getFormattedPhoneNumber(formattedPhoneNumber) {
+    this.formattedPhoneNumber = formattedPhoneNumber;
+  }
+
+  getWebsite(website) {
+    this.website = website;
+  }
+
+  getPhotos(photos) {
+    this.photos = photos;
+  }
+
+  final ScrollController listScrollController = new ScrollController();
+
+  CollectionReference collection = FirebaseFirestore.instance.collection('Places');
+  Future fetchDetails() async {
+    collection.get().then((querySnapshot) {
+      querySnapshot.docs.forEach((result) {
+        print(result.data());
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  color: Colors.blue,
-                  iconSize: 50,
-                  icon: const Icon(Icons.save),
-                  onPressed: getPlace,
-                )
-              ]),
+          child: Flexible(
+              flex: 24,
+              child: ListView.builder(
+                controller: listScrollController,
+                padding: const EdgeInsets.all(12.0),
+                itemBuilder: (context, index) {
+                  //getIt();
+                  return StreamBuilder<QuerySnapshot>(
+                    stream: collection.snapshots(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.hasError)
+                        return new Text('Error: ${snapshot.error}');
+                      switch (snapshot.connectionState) {
+                        case ConnectionState.waiting:
+                          return new Text('Loading...');
+                        default:
+                          return new ListView(
+                            shrinkWrap: true,
+                            children: snapshot.data.docs
+                                .map((DocumentSnapshot document) {
+                                  String doc = document.toString();
+                                  getPlace(doc);
+                              return new ListTile(
+                                  title: new Text(places.),
+                                  subtitle: new Text("Phone Number: " + document['phoneNumber'] + "\nEmail: "  + document['email']),
+                                  onTap: (){
+                                    firstNameField.text = document['firstName'];
+                                    lastNameField.text = document['lastName'];
+                                    phoneNumberField.text = document['phoneNumber'];
+                                    emailField.text = document['email'];
+                                    getFirstName(document['firstName']);
+                                    getLastName(document['lastName']);
+                                    getPhoneNumber(document['phoneNumber']);
+                                    getEmail(document['email']);
+                                    print(document['firstName'] + " " + document['lastName']);
+                                  }
+                              );
+                            }).toList(),
+                          );
+                      }
+                    },
+                  );
+                },
+              )),
+
         )
+
     );
   }
 
@@ -35,33 +109,26 @@ class _PlacesDetailsState extends State<PlacesDetails> {
 
 
 final places = GoogleMapsPlaces(apiKey: Platform.environment['AIzaSyAz6TJpPOpuhahblOebTaiCmtXHcipwxjc']);
-Future<void> getPlace() async{
-  var sessionToken = 'xyzabc_1234';
-  var res = await places.autocomplete('Amoeba', sessionToken: sessionToken);
+Future<PlacesDetailsResponse> getPlace(String placeID) async{
 
-  if (res.isOkay) {
-    // list autocomplete prediction
-    for (var p in res.predictions) {
-      print('- ${p.description}');
-    }
+  final places = new GoogleMapsPlaces(apiKey: "AIzaSyAz6TJpPOpuhahblOebTaiCmtXHcipwxjc");
 
-    final placeId = res.predictions.first.placeId;
-    if (placeId == null) return;
+  PlacesDetailsResponse response = await places.getDetailsByPlaceId(placeID);
 
-    // get detail of the first result
-    var details = await places.getDetailsByPlaceId(
-      placeId,
-      sessionToken: sessionToken,
-    );
+  name = response.result.name;
+  String formattedAddress = response.result.formattedAddress;
+  String formattedPhoneNumber = response.result.formattedPhoneNumber;
+  String website = response.result.website;
 
-    print('\nDetails :');
-    print(details.result.formattedAddress);
-    print(details.result.formattedPhoneNumber);
-    print(details.result.url);
-  } else {
-    print(res.errorMessage);
-  }
+  print(response.result.name);
+  print(response.result.formattedAddress);
+  print(response.result.formattedPhoneNumber);
+  print(response.result.website);
+  print(response.result.photos);
 
-  places.dispose();
+  return response;
+
 }
+
+
 
